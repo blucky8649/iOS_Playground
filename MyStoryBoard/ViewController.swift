149c8e5edb16a8a2ead6567307203bc4fe820423
blucky8649 <blucky4511@gmail.com>
2023-04-02 11:18:14 +0900

    retrieve response data with tableView

--- MyStoryBoard/ViewController.swift
+++ MyStoryBoard/ViewController.swift
@@ -12,18 +12,20 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
     
-    let data = [["Test 1-1","Test 1-2","Test 1-3","Test 1-4"],["Test 2-1","Test 2-2","Test 2-3"],["Test 3-1","Test 3-2"]]
-    
-    let articles = (0 ... 10).map { num in
-        Article(title: "title \(num)", contents: "contents \(num)", imageUrl: "https://img.freepik.com/free-photo/adorable-kitty-looking-like-it-want-to-hunt_23-2149167099.jpg?w=1480&t=st=1679828951~exp=1679829551~hmac=a532b3e12f0f888d4d968fd81fcaaf2d1ec2b0df41839042db0bb5bb66cd2a6f")
-    }
+    private var photoList: [Photo] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        MyApi.shared.callService() { r in
-            print(r.map { photo in
+        MyApi.shared.callService() { photoList in
+            print(photoList.map { photo in
                 photo.title ?? ""
             })
+            self.photoList = photoList
+            
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+            
         }
         // Do any additional setup after loading the view.
         self.view.backgroundColor = .white
@@ -38,29 +40,17 @@ class ViewController: UIViewController {
 }
 extension ViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return articles.count
+        return photoList.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let article = articles[indexPath.row]
+        let photo = photoList[indexPath.row]
         let customCell = tableView.dequeueReusableCell(withIdentifier: "customCell", for: indexPath) as! CustomCell
         
-        customCell.iv_profile.kf.setImage(with: URL(string: article.imageUrl))
-        customCell.tv_title.text = article.title
-        customCell.tv_contents.text = article.contents
+        customCell.iv_profile.kf.setImage(with: URL(string: photo.thumbnailUrl ?? ""))
+        customCell.tv_title.text = "[\(photo.id ?? 0)] album : \(photo.albumId ?? 0)"
+        customCell.tv_contents.text = photo.title
         
         return customCell
-    }
-}
-
-struct Article {
-    var title: String = ""
-    var contents: String = ""
-    var imageUrl: String = ""
-    
-    init(title: String, contents: String, imageUrl: String) {
-        self.title = title
-        self.contents = contents
-        self.imageUrl = imageUrl
     }
 }
